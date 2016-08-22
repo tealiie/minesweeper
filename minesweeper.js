8536410ce7866c3868068f0f62ef83d38b6f3163
tealiie <tealiie@live.com>
2016-08-22 13:11:50 +1200

    add checkForWin function

--- minesweeper.js
+++ minesweeper.js
@@ -22,18 +22,32 @@ function startGame () {
     board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
   }
   lib.initBoard()
+  document.addEventListener('click', checkForWin)
+  document.addEventListener('contextmenu', checkForWin)
 }
 
 // Define this function to look for a win condition:
-//
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
 function checkForWin () {
-
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
   //   lib.displayMessage('You win!')
+  for (var i = 0; i < board.cells.length; i++) {
+
+    if (board.cells[i].isMine && !board.cells[i].isMarked) {
+      return
+    }
+
+    if (!board.cells[i].isMine && board.cells[i].hidden) {
+      return
+  }
+  return lib.displayMessage('You win!')
 }
+}
+
+
+
 
 // Define this function to count the number of mines around the cell
 // (there could be as many as 8). You don't have to get the surrounding
